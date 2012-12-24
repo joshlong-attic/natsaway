dce40747031df4243477c85e8c83fa5b0dfc4309
Josh Long <josh@joshlong.com>
2012-12-24 20:42:28 +0800

    initial code.

---
+++ src/main/java/org/resouer/cloudfoundry/integration/Main.java
@@ -0,0 +1,11 @@
+package org.resouer.cloudfoundry.integration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jlong
+ * Date: 12/24/12
+ * Time: 8:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
